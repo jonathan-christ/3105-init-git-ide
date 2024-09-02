5d289e363d30a4d23838033520684feb7d28dec5
Jonathan Christ <matthewc.calaycay@gmail.com>
2024-09-02 18:38:07 +0800

    Add personalized greeting feature

--- main.js
+++ main.js
@@ -1,2 +1,7 @@
+const prompt = require("prompt-sync")()
+
 console.log("Hello World!")
 console.log("What the Sigma")
+
+const nameInput = prompt("Enter your name: ")
+console.log(`What's up my fellow skibidi rizzler, ${nameInput}`)
